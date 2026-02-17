69b3c5d591e774a5f85a7c15cb807e84cbf3a115
oscarhampart <oh230@kent.ac.uk>
2026-02-17 10:15:28 +0000

    pythag

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,14 @@
 import math  
 
 def main():
-#TO DO  
+    side1 = int(input("Input Side1: "))
+    side2 = int(input("Input Side2: "))
+    pythag(side1, side2)
 
 def pythag(A,B):
-#TO DO  
+    hyp = math.sqrt(A ** 2 + B ** 2)
+    print("The Hypotenuse is equal to: ", hyp)
 
 main()
+
+# Can also use hyp = math.hypot(A, B)
